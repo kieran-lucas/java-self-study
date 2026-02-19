96a118607b921d32c680be53482457a3bae39e8a
Kieran Lucas <kieranlucas.work@gmail.com>
2026-02-19 17:05:14 +0700

    update

---
+++ Basics/p2.java
@@ -0,0 +1,35 @@
+public class p2 {
+    static class Person {
+        int age;
+
+    }
+    public static void changeValue(int x) {
+        x = 100;
+    }
+    public static void changeAge(Person p) {
+        p.age = 100;
+    }
+    public static void changePerson(Person p) {
+        p = new Person();
+        p.age = 200;
+
+    }
+    public static void main(String[] args) {
+        int a = 10;
+        changeValue(a);
+        System.out.println(a);
+        Person p1  = new Person();
+        p1.age = 10;
+        changeAge(p1);
+        System.out.println(p1.age);
+        Person p2 = new Person();
+        p2.age = 20;
+        changePerson(p2);
+        System.out.println(p2);
+
+
+
+
+
+    }
+}
