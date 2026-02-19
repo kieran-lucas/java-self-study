a8f68936b2f32b8676c2886e70f42fb3ab53d5ad
Kieran Lucas <kieranlucas.work@gmail.com>
2026-02-19 20:20:59 +0700

    update

---
+++ Basics/p4.java
@@ -0,0 +1,18 @@
+public class p4 {
+    static class Person {
+        private int age;
+        Person(int age) {
+            if (age < 0) {
+                this.age = 0;
+            } else {
+                this.age = age;
+            }
+        }
+    }
+    public static void main(String[] args) {
+        Person p1 = new Person(-5);
+        System.out.println(p1.age);
+        Person p2 = new Person(25);
+        System.out.println(p2.age);
+    }
+}
