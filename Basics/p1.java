e099991522253b1c941dfa0f188cc1e99ce3c934
Kieran Lucas <kieranlucas.work@gmail.com>
2026-02-19 16:45:50 +0700

    update

--- Basics/p1.java
+++ Basics/p1.java
@@ -1,5 +1,6 @@
 public class p1 {
-    public static void main(String[] args) {
-        System.out.println("Hello World");
+    public void main(String[] args) {
+        System.out.println("Kieran \nComputer Science \nHello");
+
     }
 }
