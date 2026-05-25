131b0a30a77a8e198ec77a90a25285391446e554
Kieran Lucas <kieranlucas.work@gmail.com>
2026-05-25 19:45:23 +0700

    update

--- Basics/EndOfTerm/Problem 1/Main.java
+++ Basics/EndOfTerm/Problem 1/Main.java
@@ -2,7 +2,7 @@ public class Main {
     public static void main(String[] args) {
         class MyThread extends Thread {
             public void run() {
-                System.out.println("Hello from MyThread");
+                System.out.println("Hello from " + Thread.currentThread().getName());
             }
 
         }
