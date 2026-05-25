a9e4b9c1ed9e5b5562af9da604322ea2bd46beb0
Kieran Lucas <kieranlucas.work@gmail.com>
2026-05-25 20:04:14 +0700

    update

--- Basics/EndOfTerm/Problem 4/Main.java
+++ Basics/EndOfTerm/Problem 4/Main.java
@@ -1,7 +1,7 @@
 public class Main {
     public static void main(String[] args) {
         Thread myThread = new Thread(() -> {
-            System.out.println("Hello from " + Thread.currentThread().getName());
+            System.out.println("Learning Java Thread with Lambda from " + Thread.currentThread().getName());
         });
         myThread.start();
     }
