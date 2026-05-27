102f556297ce7ce84f5264570f4027c1be1749d8
Kieran Lucas <kieranlucas.work@gmail.com>
2026-05-27 14:02:16 +0700

    Update Main.java

--- Basics/EndOfTerm/Problem 13/Main.java
+++ Basics/EndOfTerm/Problem 13/Main.java
@@ -1,3 +1,17 @@
 /*
+Singleton: Bảm đảm một lớp chỉ có duy nhất một thể hiện và cung cấp
+điểm truy cập toàn cục tới thể hiện đó
+*/
 
- */
+class Singleton {
+    private static Singleton instance;
+    private Singleton() {
+
+    }
+    public static synchronized getInstance() {
+        if (instance == null) {
+            instance = new Singleton();
+        }
+        return instance;
+    }
+}
